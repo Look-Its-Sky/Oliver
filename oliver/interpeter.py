e824dab38b09c2e3ff324f116c2b3c8a355bf819
Jude Joubert <joubertjude@gmail.com>
2022-10-24 10:57:44 -0500

    commit

--- oliver/interpeter.py
+++ oliver/interpeter.py
@@ -3,19 +3,21 @@ from os import path
 import sys
 
 def checkandexec(cmd):
-    f = open("modules.txt", "r")
+    f = open(".modules", "r")
 
     #Only Supports Python3
     for x in f:
         if x.find(str("|" + cmd + "|")) != -1: #looks for parameter in modules.txt
             os.system("python3 " + x.split("| ")[1])#Found Line Executing Path
+
         f.close()
         exit()
+
     print("Command Not Found")
 
 #Gives Everything execute permissions when the program is started with --fix-permissions
 if sys.argv[1] == "--fix-permissions":
-    os.system("chmod -R +x ./modules/")
+    os.system("chmod +x ./modules/*.py")
 
 #Otherwise it checks for the command
 else:
