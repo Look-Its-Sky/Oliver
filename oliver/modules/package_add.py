9b1bdd0c67b98a1ade4496e1d4158f3f30e61b4a
Jude Joubert <joubertjude@gmail.com>
2022-11-09 21:24:04 -0600

    Add support for installing packages

---
+++ oliver/modules/package_add.py
@@ -0,0 +1,19 @@
+import sys, os
+from glob import glob
+from subprocess import call
+
+if not sys.argv[1]:
+    #note: should prob print help
+    print("Invalid Package Path")
+    exit()
+
+path = sys.argv[1]
+
+'''
+file struct:
+    <root>
+    init.py
+        resources:
+            <whatever resources are referenced>
+'''
+print("Process returned with code " + call(path + "/init.py"))
